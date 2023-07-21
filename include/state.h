1f8e1ec58354212c0428529d391315b1d829fc56
Dimitris Skondras Mexis <120742359+dimskomex@users.noreply.github.com>
2023-07-21 13:23:06 +0300

    Update state.h

--- include/state.h
+++ include/state.h
@@ -3,26 +3,63 @@
 #include "raylib.h"
 #include "ADTList.h"
 
-#define PLATFORM_NUM 20 	// how many platforms are created on the track
-#define SCREEN_WIDTH 900 	// Width of the screen
-#define SCREEN_HEIGHT 450 	// Height of the screen
+#define PLATFORM_NUM 20 		// how many platforms are created on the track
+#define SCREEN_WIDTH 900 		// Width of the screen
+#define SCREEN_HEIGHT 450 		// Height of the screen
 
-typedef enum {BALL, PLATFORM, POKEMON, MASTERBALL} ObjectType;
+typedef enum 
+{
+	BALL, 
+	PLATFORM, 
+	POKEMON
+} ObjectType;
 
-typedef enum {IDLE, JUMPING, FALLING, MOVING_UP, MOVING_DOWN} VerticalMovement;
+typedef enum 
+{
+	IDLE, 
+	JUMPING, 
+	FALLING, 
+	MOVING_UP, 
+	MOVING_DOWN
+} VerticalMovement;
 
-typedef enum {STARYU, STARMIE, BULBASAUR, CHARMANDER, PICACHU, ZEKROM, RESHIRAM, ARCEUS, REYQUAZA, TORTERRA, SNORLAX, SQUIRTLE, EEVEE, GENGAR, NOTHING} Pokemon;
+typedef enum 
+{
+	STARYU, 
+	STARMIE, 
+	BULBASAUR, 
+	CHARMANDER, 
+	PICACHU, 
+	ZEKROM, 
+	RESHIRAM, 
+	ARCEUS, 
+	REYQUAZA, 
+	TORTERRA, 
+	SNORLAX, 
+	SQUIRTLE, 
+	EEVEE, 
+	GENGAR, 
+	NOTHING
+} Pokemon;
 
+typedef enum 
+{
+	NO_POKEBALL,
+	POKEBALL, 
+	ULTRABALL, 
+	MASTERBALL
+} Pokeball; 
 
 // Information about each object
 struct object
 {
-	ObjectType type; 			// Type (Ball, Platform, Star)
-	Rectangle rect; 			// Position and size of the object. Rectangle is defined in include/raylib.h, line 213
-	VerticalMovement vert_mov; 	// Type of vertical movement
-	float vert_speed; 			// Vertical velocity
-	bool unstable; 				// (platforms only) true if unstable
-	Pokemon pokemon;			// What pokemon is it
+	ObjectType type; 				// Type (Ball, Platform, Pokemon)
+	Rectangle rect; 				// Position and size of the object. Rectangle is defined in include/raylib.h, line 213
+	VerticalMovement vert_mov; 		// Type of vertical movement
+	float vert_speed; 				// Vertical velocity
+	bool unstable; 					// (platforms only) true if unstable
+	Pokemon pokemon;				// What pokemon is it
+	Pokeball pokeball;				// What type of pokeball is it
 };
 
 typedef struct object* Object;
@@ -30,10 +67,10 @@ typedef struct object* Object;
 // General information about the state of the game
 struct state_info
 {
-	Object ball; 	// information about the ball
-	bool playing; 	// true if game is active (false after game over)
-	bool paused; 	// true if the game is paused
-	int score; 		// the current score
+	Object ball; 					// information about the ball
+	bool playing; 					// true if game is active (false after game over)
+	bool paused; 					// true if the game is paused
+	int score; 						// the current score
 };
 
 typedef struct state_info* StateInfo;
@@ -42,14 +79,12 @@ typedef struct state_info* StateInfo;
 // Information about which keys are pressed
 struct key_state
 {
-	bool up; // true if the corresponding key is pressed
-	bool down;
+	bool up; 						// true if the corresponding key is pressed
 	bool left;
 	bool right;
 	bool enter;
 	bool n;
 	bool p;
-	bool space;
 };
 
 typedef struct key_state* KeyState;
