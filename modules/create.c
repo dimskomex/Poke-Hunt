aa7132f510a2c59f6ac0bc2a0a58d8fe225f0229
dimskomex <mitsosskme@gmail.com>
2023-10-14 11:56:39 +0300

    Change coding style

--- modules/create.c
+++ modules/create.c
@@ -2,24 +2,24 @@
 
 static Pokemon create_legendary_pokemon(void)
 {
-    int x = rand() % 5;
-    return x + MEWTWO;
+        int x = rand() % 5;
+        return x + MEWTWO;
 }
 
 static Pokemon create_iconic_pokemon(void)
 {
-    int x = rand() % 7;
-    return x + GYARADOS;
+        int x = rand() % 7;
+        return x + GYARADOS;
 }
 
 static Pokemon create_classic_pokemon(void)
 {
-    int x = rand() % 8;
-    return x + STARYU;
+        int x = rand() % 8;
+        return x + STARYU;
 }
 
 Pokemon create_pokemon(void)
 {
-    int x = rand();
-    return x % 3 == 0 && x % 2 == 0 ? create_legendary_pokemon() : x % 2 == 0 ? create_iconic_pokemon() : create_classic_pokemon();
+        int x = rand();
+        return x % 3 == 0 && x % 2 == 0 ? create_legendary_pokemon() : x % 2 == 0 ? create_iconic_pokemon() : create_classic_pokemon();
 }
